33899c682d1423270806fe3c6f9df4b4fcca44db
moroz <moroz@morozs-Mac-mini.local>
2013-12-27 17:19:51 +0200

    popups

--- js/common.js
+++ js/common.js
@@ -22,16 +22,53 @@ function m_slider_action() {
 }
 m_slider_action();
 
+//select
+function select() {
+	var el = $('.js-select');
+	el.find('.select__value').bind('click', function(){		
+		if ($(this).parent().hasClass('is-open')) {
+			$(this).parent().removeClass('is-open');
+			$(this).next().hide();
+		}
+		else {
+			el.removeClass('is-open');
+			el.find('.select__options').hide();
+			$(this).parent().addClass('is-open');
+			$(this).next().slideDown();
+		}
+	})
+	el.find('.select__options li').bind('click', function(){
+		var val = $(this).text();
+		$(this).parent().prev().html(val);
+		$(this).parent().next().val(val);
+		$(this).parent().hide();
+		$(this).parent().parent().removeClass('is-open');
+	})
+	el.click(function(event){
+		event.stopPropagation();
+	});
+	$(document).click(function() {
+		el.find('.select__options').hide();
+		el.removeClass('is-open');
+	});
+}
+select();
+
 //phone order
 var btn_order_phone = $('.js-phone-order');
 btn_order_phone.bind('click', function() {
 	$(this).next().fadeToggle(200);
 });
 
-
 //go top
 $('.js-go-top').bind('click', function(){
 	$('body').animate({scrollTop: 0}, 800);
+});
+
+//service more
+$('.js-serice-more').bind('click', function(){
+	$('.service').toggleClass('is-open');
+	$(this).toggleClass('is-active');
 });
 
 //window click
